9c5322e8f57533aa7a5eef93643c90e99c6d34b5
Marcos Minond <minond.marcos@gmail.com>
2015-07-27 23:20:34 -0600

    stand-alone sections

--- toc.js
+++ toc.js
@@ -5,23 +5,48 @@ angular.module('rector').directive('rectorToc', function () {
         template: '<div class="rector-toc--items"></div>',
         link: function (scope, elem) {
             var $items = elem.find('.rector-toc--items'),
-                $body = angular.element('html, body');
+                $body = angular.element('html, body'),
+                $home,
+                $last;
+
+            function scroll_to($section) {
+                $body.animate({
+                    scrollTop: $section.offset().top - 20
+                }, 0, 'swing');
+            }
+
+            function show($section) {
+                if ($section === $last) {
+                    return;
+                } else if ($section.attr('rector-stand-alone') !== undefined) {
+                    angular.element('rector-section').parent().fadeOut();
+                    $section.parent().fadeIn();
+                } else {
+                    angular.element('rector-section').parent().fadeIn();
+                    scroll_to($section);
+                }
+
+                $last = $section;
+            }
 
             angular.element('rector-section').each(function () {
                 var $this = angular.element(this),
                     label = $this.text();
 
+                $home = $home || $this;
                 angular.element('<div></div>')
                     .text(label)
                     .appendTo($items)
                     .addClass('rector-toc--item')
                     .attr('tabindex', '0')
                     .on('click keypress', function () {
-                        $body.animate({
-                            scrollTop: $this.offset().top - 30
-                        }, 700, 'swing');
+                        show($this);
                     });
             });
+
+            if ($home) {
+                show($home);
+            }
         }
     };
 });
